dee8795d7d3c3bfe8d1f28de680cb7fc487c5f8c
ayushiarya-00 <ayushiarya190700@gmail.com>
2021-05-13 22:49:56 +0530

    Second Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+ window.onscroll = function() {myFunction()};
+    
+ var header = document.getElementById("myHeader");
+ var sticky = header.offsetTop;
+ 
+ function myFunction() {
+   if (window.pageYOffset > sticky) {
+     header.classList.add("sticky");
+   } else {
+     header.classList.remove("sticky");
+   }
+ }
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
